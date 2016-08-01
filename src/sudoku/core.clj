392cd25bc383afe9a84bc212256a6fe43b52b864
Adonis Bovell <adonis.bovell@damballa.com>
2016-08-01 08:43:46 -0400

    Get rows, columns squares by indices

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -77,10 +77,93 @@
    (def first-chunk (range start (+ start grouping)))
    (sort (flatten (map #(range % (+ % (* rows grouping)) rows) first-chunk)))))
 
+(defn except
+  "Remove a value from list"
+  [index values]
+  (remove #(= index %) values))
+
+(defn neighbors-ind
+  "Generate indices of neighbors given piece index"
+  ([index]
+   (neighbors-ind index 81))
+  ([index max-pos]
+   (except index (sort (set (reduce into [(row-ind (row-of index max-pos) max-pos)
+                                                 (col-ind (col-of index max-pos) max-pos)
+                                                 (square-ind (square-of index max-pos) max-pos)]))))))
+
+(defn solved?
+  "Check if value of piece is known"
+  [puzzle index]
+  (not (= 0 (nth puzzle index))))
+
 (defn puzzle-pieces
   "Get puzzle pieces by list of indices"
   [puzzle indices]
-  (mapv puzzle indices))
+  (map nth (repeat puzzle) indices))
+
+(defn get-neighbors
+  "Get neighboring piece values"
+  ([of-function ind-function puzzle index max-pos]
+   (puzzle-pieces puzzle (except index (ind-function (of-function index max-pos)))))
+  )
+
+(defn row-neighbors
+  "Get values from neighboring cells"
+  ([puzzle index]
+   (row-neighbors puzzle index 81))
+  ([puzzle index max-pos]
+   (get-neighbors row-of row-ind puzzle index max-pos)))
+
+(defn col-neighbors
+  ([puzzle index]
+   (col-neighbors puzzle index 81))
+  ([puzzle index max-pos]
+   (get-neighbors col-of col-ind puzzle index max-pos)))
+
+(defn square-neighbors
+  ([puzzle index]
+   (square-neighbors puzzle index 81))
+  ([puzzle index max-pos]
+   (get-neighbors square-of square-ind puzzle index max-pos)))
+
+(defn neighbors
+  ([puzzle index]
+   (neighbors puzzle index 81))
+  ([puzzle index max-pos]
+   (puzzle-pieces puzzle (neighbors-ind index max-pos))))
+
+(defn all-choices
+  ([]
+   (all-choices 81))
+  ([max-pos]
+   (set (range 1 (inc (math/sqrt max-pos))))))
+
+(defn choices
+  ([puzzle index]
+   (choices puzzle index 81))
+  ([puzzle index max-pos]
+   (clojure.set/difference (all-choices max-pos) (set (neighbors puzzle index max-pos)))))
+
+(defn candidate-pieces
+  "Generate a list of possible guesses"
+  [puzzle]
+  (def n-pieces (count puzzle))
+  (def solutions #(if (solved? puzzle %) nil (choices puzzle %)))
+  (def possible-solutions (zipmap (range 0 n-pieces)
+                                  (map solutions (range 0 n-pieces))))
+  (remove #(= nil (last %)) (sort-by #(count (last %)) possible-solutions)))
+
+(defn new-puzzles-from-choices
+  "Generate new puzzles with guessed fields"
+  [puzzle [index choices]]
+  (def puzzle-list (into [] puzzle))
+  (map #(seq (assoc %1 %2 %3)) (repeat puzzle-list) (repeat index) choices)
+)
+
+(defn valid?
+  [puzzle index]
+  ;; TODO
+)
 
 (defn read-puzzle
   "Transform text puzzle into an seq on integers"
@@ -98,12 +181,16 @@
   (conj (map row-to-string rows (repeat groups))
         (row-to-string (take columns (repeat "-")) groups)))
 
-(defn puzzle-to-string
+(defn puzzle-to-print-string
   [puzzle]
   (def rows (partition (row-size puzzle) puzzle))
   (def row-groups (partition (group-size puzzle) rows))
-  (string/join "\n" (flatten (map row-group-to-string row-groups))))
+  (string/replace (string/join "\n" (flatten (map row-group-to-string row-groups))) #"0" "*"))
 
 (defn print-puzzle
   [puzzle]
-  (print (puzzle-to-string puzzle)))
+  (print (puzzle-to-print-string puzzle)))
+
+(defn puzzle-to-string
+  [puzzle]
+  (string/join "" puzzle))
